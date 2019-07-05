e6d933e427166a591dc0ba10107f0b7bf162b025
Julian van de Groep <me@flash.moe>
2019-07-05 05:25:29 +0200

    adaf

---
+++ SquidChat/Program.cs
@@ -0,0 +1,60 @@
+﻿using Fleck;
+using System;
+
+namespace SquidChat
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("SquidChat - Multi-user (PHP) Sock Chat");
+
+            WebSocketServer srv = new WebSocketServer("ws://0.0.0.0:6770");
+            srv.Start(s =>
+            {
+                s.OnOpen = () => OnOpen(s);
+                s.OnClose = () => OnClose(s);
+                s.OnError = err => OnError(s, err);
+                s.OnMessage = msg => OnMessage(s, msg);
+            });
+            Console.ReadLine();
+        }
+
+        private static void OnOpen(IWebSocketConnection conn)
+        {
+            Console.WriteLine($@"[{conn.ConnectionInfo.ClientIpAddress}] Open");
+        }
+
+        private static void OnClose(IWebSocketConnection conn)
+        {
+            Console.WriteLine($@"[{conn.ConnectionInfo.ClientIpAddress}] Close");
+        }
+
+        private static void OnError(IWebSocketConnection conn, Exception ex)
+        {
+            Console.WriteLine($@"[{conn.ConnectionInfo.ClientIpAddress}] Err {ex}");
+        }
+
+        private static void OnMessage(IWebSocketConnection conn, string msg)
+        {
+            Console.WriteLine($@"[{conn.ConnectionInfo.ClientIpAddress}] {msg}");
+
+            string[] args = msg.Split('\t');
+
+            if (args.Length < 1 || !Enum.TryParse(args[0], out SockChatServerMessage opCode))
+                return;
+
+            switch(opCode)
+            {
+                case SockChatServerMessage.Ping:
+                    break;
+
+                case SockChatServerMessage.Authenticate:
+                    break;
+
+                case SockChatServerMessage.MessageSend:
+                    break;
+            }
+        }
+    }
+}
---
+++ SquidChat/SockChatEnums.cs
@@ -0,0 +1,61 @@
+﻿namespace SquidChat
+{
+    public enum SockChatServerMessage
+    {
+        Ping = 0,
+        Authenticate = 1,
+        MessageSend = 2,
+    }
+
+    public enum SockChatClientMessage
+    {
+        Pong = 0,
+        UserConnect = 1,
+        MessageAdd = 2,
+        UserDisconnect = 3,
+        ChannelEvent = 4,
+        UserSwitch = 5,
+        MessageDelete = 6,
+        ContextPopulate = 7,
+        ContextClear = 8,
+        ForceDisconnect = 9,
+        UserUpdate = 10,
+    }
+
+    enum SockChatChannelEventType
+    {
+        Create = 0,
+        Update = 1,
+        Delete = 2,
+    }
+
+    // Force is only ever sent directly to a user, the rest is broadcast to the channel
+    enum SockChatUserSwitchType
+    {
+        Join = 0,
+        Leave = 1,
+        Force = 2,
+    }
+
+    enum SockChatContextPopulateType
+    {
+        Users = 0,
+        Message = 1,
+        Channels = 2,
+    }
+
+    enum SockChatContextClearType
+    {
+        Messages = 0,
+        Users = 1,
+        Channels = 2,
+        MessagesUsers = 3,
+        All = 4,
+    }
+
+    enum SockChatForceDisconnectType
+    {
+        Kicked = 0,
+        Banned = 1,
+    }
+}
