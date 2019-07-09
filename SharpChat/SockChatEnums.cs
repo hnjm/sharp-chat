57839741494440dd17930fe4ec3de4bed78e907e
Julian van de Groep <me@flash.moe>
2019-07-09 16:59:20 +0200

    removed unused enums

--- SharpChat/SockChatEnums.cs
+++ SharpChat/SockChatEnums.cs
@@ -21,35 +21,4 @@
         BAKA = 9,
         UserUpdate = 10,
     }
-
-    enum SockChatChannelEventType
-    {
-        Create = 0,
-        Update = 1,
-        Delete = 2,
-    }
-
-    // Force is only ever sent directly to a user, the rest is broadcast to the channel
-    enum SockChatUserSwitchType
-    {
-        Join = 0,
-        Leave = 1,
-        Force = 2,
-    }
-
-    enum SockChatContextPopulateType
-    {
-        Users = 0,
-        Message = 1,
-        Channels = 2,
-    }
-
-    enum SockChatContextClearType
-    {
-        Messages = 0,
-        Users = 1,
-        Channels = 2,
-        MessagesUsers = 3,
-        All = 4,
-    }
 }
