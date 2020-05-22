90c0081834aa78ae5c92ec852758e241bfd4ec28
flashwave <me@flash.moe>
2020-05-22 15:25:04 +0200

    Added debug utility for Misuzu endpoints.

--- SharpChat/Program.cs
+++ SharpChat/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using SharpChat.Flashii;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 
 namespace SharpChat {
@@ -16,10 +19,59 @@ namespace SharpChat {
             Console.WriteLine(@"============================================ DEBUG ==");
 #endif
 
+#if DEBUG
+            Console.WriteLine(@"HOLD A KEY TO START A TEST NOW");
+            Thread.Sleep(1000);
+            if (Console.KeyAvailable)
+                switch (Console.ReadKey(true).Key) {
+                    case ConsoleKey.F:
+                        TestMisuzuAuth();
+                        return;
+                }
+#endif
+
             using ManualResetEvent mre = new ManualResetEvent(false);
             using SockChatServer scs = new SockChatServer(PORT);
             Console.CancelKeyPress += (s, e) => { e.Cancel = true; mre.Set(); };
             mre.WaitOne();
         }
+
+#if DEBUG
+        private static void TestMisuzuAuth() {
+            Console.WriteLine($@"Enter token found on {FlashiiUrls.BASE_URL}/login:");
+            string[] token = Console.ReadLine().Split('_');
+
+            FlashiiAuth authRes = FlashiiAuth.Attempt(new FlashiiAuthRequest {
+                UserId = int.Parse(token[0]), Token = token[1], IPAddress = @"1.2.4.8"
+            });
+
+            if(authRes.Success) {
+                Console.WriteLine(@"Auth success!");
+                Console.WriteLine($@" User ID:   {authRes.UserId}");
+                Console.WriteLine($@" Username:  {authRes.Username}");
+                Console.WriteLine($@" Colour:    {authRes.ColourRaw:X8}");
+                Console.WriteLine($@" Hierarchy: {authRes.Hierarchy}");
+                Console.WriteLine($@" Silenced:  {authRes.SilencedUntil}");
+                Console.WriteLine($@" Perms:     {authRes.Permissions}");
+            } else {
+                Console.WriteLine($@"Auth failed: {authRes.Reason}");
+                return;
+            }
+
+            Console.WriteLine(@"Bumping last seen...");
+            FlashiiBump.Submit(new[] { new ChatUser(authRes) });
+
+            Console.WriteLine(@"Fetching ban list...");
+            IEnumerable<FlashiiBan> bans = FlashiiBan.GetList();
+            Console.WriteLine($@"{bans.Count()} BANS");
+            foreach(FlashiiBan ban in bans) {
+                Console.WriteLine($@"BAN INFO");
+                Console.WriteLine($@" User ID:    {ban.UserId}");
+                Console.WriteLine($@" Username:   {ban.Username}");
+                Console.WriteLine($@" IP Address: {ban.UserIP}");
+                Console.WriteLine($@" Expires:    {ban.Expires}");
+            }
+        }
+#endif
     }
 }
