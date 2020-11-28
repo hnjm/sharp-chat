ddac9ae23696f29b32fc1cd89c3ef1e2dbdbe2fb
Julian van de Groep <me@flash.moe>
2020-11-28 18:22:31 +0100

    SharpChatTest project makeover!

---
+++ SharpChatTest/SharpChatExec.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace SharpChatTest {
+    public class SharpChatExec : IDisposable {
+        private Process Process { get; }
+
+        public SharpChatExec(ushort port, string dbPath) {
+            Process = Process.Start(new ProcessStartInfo {
+                Arguments = string.Format(@"--dpn null --dbb sqlite --dbpath ""{1}"" --ip 127.0.0.1 --port {0} --testmode", port, dbPath),
+                CreateNoWindow = false,
+                FileName = GetExePath(),
+                UseShellExecute = false,
+                WorkingDirectory = Directory.GetCurrentDirectory(),
+                RedirectStandardInput = true,
+            });
+            Thread.Sleep(1000);
+        }
+
+        private static string GetExePath() {
+            string path = Directory.GetCurrentDirectory();
+            string target = Path.GetFileName(path);
+
+            while(!File.Exists(Path.Combine(path, @"SharpChat.sln")))
+                path = Path.GetDirectoryName(path);
+
+            path = Path.Combine(
+                path, @"SharpChat/bin/Debug", target,
+                RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? @"SharpChat.exe" : @"SharpChat"
+            );
+
+            return path;
+        }
+
+        private bool IsDisposable;
+
+        ~SharpChatExec()
+            => DoDispose();
+
+        public void Dispose() {
+            DoDispose();
+            GC.SuppressFinalize(this);
+        }
+
+        private void DoDispose() {
+            if(IsDisposable)
+                return;
+            IsDisposable = true;
+
+            Process.StandardInput.WriteLine('\x3');
+            Process.StandardInput.Flush();
+            Process.WaitForExit();
+        }
+    }
+}
---
+++ SharpChatTest/SockChat/SockChatClient.cs
@@ -0,0 +1,63 @@
+﻿using PureWebSockets;
+using System;
+using System.Net.WebSockets;
+
+namespace SharpChatTest.SockChat {
+    public class SockChatClient : IDisposable {
+        public string SpoofedIP { get; }
+        private PureWebSocket WebSocket { get; }
+
+        public int UserId { get; }
+        public bool IsConnected { get; private set; }
+
+        public SockChatClient(ushort port, int userId) {
+            UserId = userId;
+            Random rng = new Random(userId);
+            SpoofedIP = rng.Next(1, 254).ToString() + '.' + rng.Next(1, 254) + '.' + rng.Next(1, 254) + '.' + rng.Next(1, 254);
+            WebSocket = new PureWebSocket($@"ws://127.0.0.1:{port}/", new PureWebSocketOptions {
+                Headers = new Tuple<string, string>[] {
+                    new Tuple<string, string>(@"X-Real-IP", SpoofedIP),
+                },
+            });
+            WebSocket.OnOpened += WebSocket_OnOpened;
+            WebSocket.OnClosed += WebSocket_OnClosed;
+            WebSocket.OnError += WebSocket_OnError;
+            WebSocket.OnMessage += WebSocket_OnMessage;
+            WebSocket.Connect();
+        }
+
+        private void WebSocket_OnOpened(object sender) {
+            IsConnected = true;
+        }
+
+        private void WebSocket_OnClosed(object sender, WebSocketCloseStatus reason) {
+            IsConnected = false;
+        }
+
+        private void WebSocket_OnError(object sender, Exception ex) {
+            //
+        }
+
+        private void WebSocket_OnMessage(object sender, string message) {
+            //
+        }
+
+        private bool IsDisposed;
+
+        ~SockChatClient()
+            => DoDispose();
+
+        public void Dispose() {
+            DoDispose();
+            GC.SuppressFinalize(this);
+        }
+
+        private void DoDispose() {
+            if(IsDisposed)
+                return;
+            IsDisposed = true;
+
+            WebSocket.Dispose();
+        }
+    }
+}
