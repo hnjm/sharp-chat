0d2327a93a2f95f249696bacd6ee71d2d7e4c221
Julian <me@flash.moe>
2020-11-29 02:19:15 +0100

    Add ability to simulate authentication failures.

--- SharpChat.Common/DataProvider/Null/NullUserAuthClient.cs
+++ SharpChat.Common/DataProvider/Null/NullUserAuthClient.cs
@@ -3,6 +3,8 @@
 namespace SharpChat.DataProvider.Null {
     public class NullUserAuthClient : IUserAuthClient {
         public IUserAuthResponse AttemptAuth(UserAuthRequest request) {
+            if(request.Token.StartsWith(@"FAIL:"))
+                throw new UserAuthFailedException(request.Token[5..]);
             return new NullUserAuthResponse(request);
         }
     }
